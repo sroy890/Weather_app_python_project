ee2aeb8c703e462749ed5e1e9939e59402445034
Subhajit <s.roy8902718865@gmail.com>
2024-04-13 03:19:41 +0530

    Created Weather app project in pythn

---
+++ weather.py
@@ -0,0 +1,24 @@
+import requests
+from dotenv import load_dotenv
+import os
+from pprint import pprint
+
+load_dotenv()
+
+def current_weather():
+    print('\n*** Getting Current Weather Condition*** 🌡️🌡️🌡️\n')
+    city = input('\nPlease Enter a city name:\n')
+    
+    request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
+    
+    # print(request_url)
+    weather_details = requests.get(request_url).json()
+    # print(weather_details)
+    # pprint(weather_details)
+    print(f'\nCurrent weather for {weather_details["name"]}')
+    print(f'\nThe Temperature is for {weather_details["main"]["temp"]}')
+    print(f'\nFeels like {weather_details["main"]["feels_like"]} and {weather_details["weather"][0]["description"]}')
+       
+    
+if __name__ == "__main__":   
+    current_weather()
